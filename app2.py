94605dfe677a2bf2cf049deef2148d08066eb8e2
JavierUrreaFucs <jhurrea@fucsalud.edu.co>
2024-10-17 13:59:30 -0500

    Genrar qr activo

--- app2.py
+++ app2.py
@@ -2,22 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, send_file,
 from flask_sqlalchemy import SQLAlchemy
 import qrcode
 from io import BytesIO
-#from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# Configuración de la base de datos MySQL
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = ''
-#app.config['MYSQL_DB'] = 'contact_db'
-#
-#mysql = MySQL(app)
-
-# Obtén la URL de la base de datos desde la variable de entorno
+# Configuración de la base de datos PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://contact_db_uicb_user:6Nf9WYZDrfH6ot5014l9e3CRu8YCf6ZO@dpg-cs8i9idsvqrc73bs3s1g-a.oregon-postgres.render.com/contact_db_uicb'
 
-# Inicializa la base de datos
 db = SQLAlchemy(app)
 
 # Define tus modelos aquí
@@ -40,7 +30,6 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -57,18 +46,6 @@ def add_contact():
         address = request.form['address']
         org = request.form['org']
 
-        # Insertar los datos en la base de datos con el status 'active'
-        #cursor = mysql.connection.cursor()
-        #cursor.execute("""
-        #    INSERT INTO contacts (nombre1, nombre2, apellido1, apellido2, phone, email, address, org, status)
-        #    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, 'active')
-        #""", (nombre1, nombre2, apellido1, apellido2, phone, email, address, org))
-        #mysql.connection.commit()
-        #contact_id = cursor.lastrowid
-        #cursor.close()
-        #return redirect(url_for('generate_qr', contact_id=contact_id))
-
-        # Crear un nuevo usuario usando el modelo
         new_contact = User(
             nombre1=nombre1,
             nombre2=nombre2,
@@ -81,7 +58,6 @@ def add_contact():
             status='active'
         )
 
-        # Agregar a la base de datos
         db.session.add(new_contact)
         db.session.commit()
 
@@ -89,17 +65,22 @@ def add_contact():
 
 @app.route('/generate_qr/<int:contact_id>')
 def generate_qr(contact_id):
-    # Generar la URL que será codificada en el QR
-    url_contacto = url_for('view_contact', contact_id=contact_id, _external=True)
+    contact = User.query.get(contact_id)
 
-    # Generar el código QR con la URL
+    if contact is None or contact.status != 'active':
+        return "El contacto no está disponible o no está activo.", 403
+
+    # Generar la URL para obtener el vCard del contacto
+    vcard_url = url_for('view_contact', contact_id=contact_id, _external=True)
+
+    # Generar el código QR con la URL del vCard
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(url_contacto)
+    qr.add_data(vcard_url)
     qr.make(fit=True)
 
     qr_img = qr.make_image(fill='black', back_color='white')
@@ -114,15 +95,10 @@ def generate_qr(contact_id):
 
 @app.route('/contact_info/<int:contact_id>')
 def view_contact(contact_id):
-    # Recuperar los datos del contacto desde la base de datos, incluyendo el status
-    #cursor = mysql.connection.cursor()
-    #cursor.execute("SELECT nombre1, nombre2, apellido1, apellido2, phone, email, address, org, status FROM contacts WHERE id = %s", [contact_id])
-    #contact = cursor.fetchone()
-    #cursor.close()
     contact = User.query.get(contact_id)
 
-    if contact is None:
-        return "Contacto no encontrado", 404
+    if contact is None or contact.status != 'active':
+        return "El contacto ya no está disponible.", 403
 
     nombre1 = contact.nombre1
     nombre2 = contact.nombre2
@@ -132,22 +108,17 @@ def view_contact(contact_id):
     email = contact.email
     address = contact.address
     org = contact.org
-    status = contact.status
 
-    # Verificar si el contacto está activo
-    if status != 'active':
-        return "El contacto ya no está disponible.", 403
-
-    # Crear el archivo vCard
+    # Crear el archivo vCard dinámicamente
     vcard = f"""BEGIN:VCARD
-      VERSION:3.0
-      N:{apellido2};{nombre2};{apellido1};{nombre1}
-      FN:{nombre1} {nombre2} {apellido1} {apellido2}
-      TEL;TYPE=WORK,VOICE:+57{phone}
-      EMAIL:{email}
-      ORG:{org}
-      ADR;TYPE=work:;;{address};;;;
-      END:VCARD"""
+    VERSION:3.0
+    N:{apellido2};{nombre2};{apellido1};{nombre1}
+    FN:{nombre1} {nombre2} {apellido1} {apellido2}
+    TEL;TYPE=WORK,VOICE:{phone}
+    EMAIL:{email}
+    ORG:{org}
+    ADR;TYPE=work:;;{address};;;;
+    END:VCARD"""
 
     # Crear la respuesta con el archivo vCard
     response = make_response(vcard)
